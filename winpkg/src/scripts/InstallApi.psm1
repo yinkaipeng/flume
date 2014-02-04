f17f6cb342b41dca4a44e4819f38636554b05ea6
Raja Aluri <raja@cmbasics.com>
2014-02-04 11:02:52 -0800

    WINPKG CHANGES: These are all the winpkg related changes from monarch.
        We are bringing in as one commit. This is a squashed commit of the following
        commits. This will skew the author information, but for the real author
        information, please use the commits that are showing in as cherry-picks
        from monarch repo.
    
    0c5c898 FLUME-2219 : Windows: Flume tests need to know location of
    Hadoop native libraries (hadoop.dll)
    2d75139 Disabling morphline sink module from build
    41c879b [FLUME-10] Adding ZIP to the build distribution
    6825125 Briging in condor winpkg changes into bigwheel
    be76803 Bringing in condor winpkg changes
    cd9159d HADOOP-699 replace servicehost.exe binaries
    d31c804 FLUME-2127 Add JMX Shutdown command
    (cherry picked from commit c37534b28374e61ebbc073a83f530dd942c43e02)
    
    BUG-14101 Make winpkg module build profile dependent, so it does not run on Unix builds automatically
    (cherry picked from commit 10487e7ab533e6f7d2a075d7b83fa2db329b390a)
    
    BUG-14101 Make winpkg module build profile dependent, so it does not run on Unix builds automatically
    (cherry picked from commit b02420ce12f120483e4159d7dcd7447fbcb7fc84)
    
    BUG-15013 Services not recreated if they exists before installation.
    (cherry picked from commit 251781bf986afef28b0a805825a3cba081099b6c)
    
    Fixing the name of winpkg profile
    (cherry picked from commit 43c06ca0411abe523ff8050de4e935b7a8d84638)

---
+++ winpkg/src/scripts/InstallApi.psm1
@@ -0,0 +1,464 @@
+﻿### Licensed to the Apache Software Foundation (ASF) under one or more
+### contributor license agreements.  See the NOTICE file distributed with
+### this work for additional information regarding copyright ownership.
+### The ASF licenses this file to You under the Apache License, Version 2.0
+### (the "License"); you may not use this file except in compliance with
+### the License.  You may obtain a copy of the License at
+###
+###     http://www.apache.org/licenses/LICENSE-2.0
+###
+### Unless required by applicable law or agreed to in writing, software
+### distributed under the License is distributed on an "AS IS" BASIS,
+### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+### See the License for the specific language governing permissions and
+### limitations under the License.
+
+###
+### A set of basic PowerShell routines that can be used to install and
+### manage Hadoop services on a single node. For use-case see install.ps1.
+###
+
+###
+### Global variables
+###
+$ScriptDir = Resolve-Path (Split-Path $MyInvocation.MyCommand.Path)
+
+$FinalName = "flume-@flume.version@"
+
+###############################################################################
+###
+### Installs Flume.
+###
+### Arguments:
+###     component: Component to be installed, it can be "core, "hdfs" or "mapreduce"
+###     nodeInstallRoot: Target install folder (for example "C:\Hadoop")
+###     serviceCredential: Credential object used for service creation
+###     role: Space separated list of roles that should be installed.
+###           (for example, "jobtracker historyserver" for mapreduce)
+###
+###############################################################################
+
+function Install(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $component,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $nodeInstallRoot,
+    [System.Management.Automation.PSCredential]
+    [Parameter( Position=2, Mandatory=$false )]
+    $serviceCredential,
+    [String]
+    [Parameter( Position=3, Mandatory=$false )]
+    $roles
+    )
+{
+
+
+    if ( $component -eq "flume" )
+    {
+        $HDP_INSTALL_PATH, $HDP_RESOURCES_DIR = Initialize-InstallationEnv $scriptDir "$FinalName.winpkg.log"
+        Write-Log "Checking the JAVA Installation."
+        if( -not (Test-Path $ENV:JAVA_HOME\bin\java.exe))
+        {
+            Write-Log "JAVA_HOME not set properly; $ENV:JAVA_HOME\bin\java.exe does not exist" "Failure"
+            throw "Install: JAVA_HOME not set properly; $ENV:JAVA_HOME\bin\java.exe does not exist."
+        }
+
+        Write-Log "Checking the Hadoop Installation."
+        if( -not (Test-Path $ENV:HADOOP_HOME\bin\winutils.exe))
+        {
+
+          Write-Log "HADOOP_HOME not set properly; $ENV:HADOOP_HOME\bin\winutils.exe does not exist" "Failure"
+          throw "Install: HADOOP_HOME not set properly; $ENV:HADOOP_HOME\bin\winutils.exe does not exist."
+        }
+
+	    ### $flumeInstallPath: the name of the folder containing the application, after unzipping
+	    $flumeInstallPath = Join-Path $nodeInstallRoot $FinalName
+		$flumeInstallToBin = Join-Path "$flumeInstallPath" "bin"
+
+	    Write-Log "Installing Apache $FinalName to $flumeInstallPath"
+
+        ### Create Node Install Root directory
+        if( -not (Test-Path "$flumeInstallPath"))
+        {
+            Write-Log "Creating Node Install Root directory: `"$flumeInstallPath`""
+            $cmd = "mkdir `"$flumeInstallPath`""
+            Invoke-CmdChk $cmd
+        }
+
+		# Rename zip file and initialize parent directory of $flumeInstallPath
+		Rename-Item "$HDP_RESOURCES_DIR\apache-$FinalName-bin.zip" "$HDP_RESOURCES_DIR\$FinalName.zip"
+		$flumeIntallPathParent = (Get-Item $flumeInstallPath).parent.FullName
+
+        ###
+        ###  Unzip Flume distribution from compressed archive
+        ###
+
+        Write-Log "Extracting $FinalName.zip to $flumeIntallPathParent"
+        if ( Test-Path ENV:UNZIP_CMD )
+        {
+            ### Use external unzip command if given
+            $unzipExpr = $ENV:UNZIP_CMD.Replace("@SRC", "`"$HDP_RESOURCES_DIR\$FinalName.zip`"")
+            $unzipExpr = $unzipExpr.Replace("@DEST", "`"$flumeIntallPathParent`"")
+            ### We ignore the error code of the unzip command for now to be
+            ### consistent with prior behavior.
+            Invoke-Ps $unzipExpr
+        }
+        else
+        {
+            $shellApplication = new-object -com shell.application
+            $zipPackage = $shellApplication.NameSpace("$HDP_RESOURCES_DIR\$FinalName.zip")
+            $destinationFolder = $shellApplication.NameSpace($flumeInstallPath)
+            $destinationFolder.CopyHere($zipPackage.Items(), 20)
+        }
+
+		#Rename apache-$FinalName-bin to $FinalName
+		Remove-Item "$flumeIntallPathParent\$FinalName"
+		Move-Item -Path "$flumeIntallPathParent\apache-$FinalName-bin" -destination "$flumeIntallPathParent\$FinalName"
+
+        ###
+        ### Set FLUME_HOME environment variable
+        ###
+        Write-Log "Setting the FLUME_HOME environment variable at machine scope to `"$flumeInstallPath`""
+        [Environment]::SetEnvironmentVariable("FLUME_HOME", $flumeInstallPath, [EnvironmentVariableTarget]::Machine)
+        $ENV:FLUME_HOME = "$flumeInstallPath"
+
+		if ($roles) {
+
+		###
+		### Create flume Windows Services and grant user ACLS to start/stop
+		###
+		Write-Log "Node flume Role Services: $roles"
+
+		### Verify that roles are in the supported set
+		CheckRole $roles @("flumeagent")
+		Write-Log "Role : $roles"
+		foreach( $service in empty-null ($roles -Split('\s+')))
+		{
+			CreateAndConfigureHadoopService $service $HDP_RESOURCES_DIR $flumeInstallToBin $serviceCredential
+		}
+
+	     ### end of roles loop
+        }
+	    Write-Log "Finished installing Apache flume"
+    }
+    else
+    {
+        throw "Install: Unsupported component argument."
+    }
+}
+
+
+###############################################################################
+###
+### Uninstalls Hadoop component.
+###
+### Arguments:
+###     component: Component to be uninstalled, it can be "core, "hdfs" or "mapreduce"
+###     nodeInstallRoot: Install folder (for example "C:\Hadoop")
+###
+###############################################################################
+
+function Uninstall(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $component,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $nodeInstallRoot
+    )
+{
+    if ( $component -eq "flume" )
+    {
+        $HDP_INSTALL_PATH, $HDP_RESOURCES_DIR = Initialize-InstallationEnv $scriptDir "$FinalName.winpkg.log"
+
+	    Write-Log "Uninstalling Apache flume $FinalName"
+	    $flumeInstallPath = Join-Path $nodeInstallRoot $FinalName
+
+        ### If Hadoop Core root does not exist exit early
+        if ( -not (Test-Path $flumeInstallPath) )
+        {
+            return
+        }
+
+		### Stop and delete services
+        ###
+        foreach( $service in ("flumeagent"))
+        {
+            StopAndDeleteHadoopService $service
+        }
+
+	    ###
+	    ### Delete install dir
+	    ###
+	    $cmd = "rd /s /q `"$flumeInstallPath`""
+	    Invoke-Cmd $cmd
+
+        ### Removing FLUME_HOME environment variable
+        Write-Log "Removing the FLUME_HOME environment variable"
+        [Environment]::SetEnvironmentVariable( "FLUME_HOME", $null, [EnvironmentVariableTarget]::Machine )
+
+        Write-Log "Successfully uninstalled flume"
+
+    }
+    else
+    {
+        throw "Uninstall: Unsupported compoment argument."
+    }
+}
+
+###############################################################################
+###
+### Start component services.
+###
+### Arguments:
+###     component: Component name
+###     roles: List of space separated service to start
+###
+###############################################################################
+function StartService(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $component,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $roles
+    )
+{
+    Write-Log "Starting `"$component`" `"$roles`" services"
+
+    if ( $component -eq "flume" )
+    {
+        Write-Log "StartService: flume services"
+		CheckRole $roles @("flumeagent")
+
+        foreach ( $role in $roles -Split("\s+") )
+        {
+            Write-Log "Starting $role service"
+            Start-Service $role
+        }
+    }
+    else
+    {
+        throw "StartService: Unsupported component argument."
+    }
+}
+
+###############################################################################
+###
+### Stop component services.
+###
+### Arguments:
+###     component: Component name
+###     roles: List of space separated service to stop
+###
+###############################################################################
+function StopService(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $component,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $roles
+    )
+{
+    Write-Log "Stopping `"$component`" `"$roles`" services"
+
+    if ( $component -eq "flume" )
+    {
+        ### Verify that roles are in the supported set
+        CheckRole $roles @("flumeagent")
+        foreach ( $role in $roles -Split("\s+") )
+        {
+            try
+            {
+                Write-Log "Stopping $role "
+                if (Get-Service "$role" -ErrorAction SilentlyContinue)
+                {
+                    Write-Log "Service $role exists, stopping it"
+                    Stop-Service $role
+                }
+                else
+                {
+                    Write-Log "Service $role does not exist, moving to next"
+                }
+            }
+            catch [Exception]
+            {
+                Write-Host "Can't stop service $role"
+            }
+
+        }
+    }
+    else
+    {
+        throw "StartService: Unsupported compoment argument."
+    }
+}
+
+###############################################################################
+###
+### Alters the configuration of the flume component.
+###
+### Arguments:
+###     component: Component to be configured, it should be "flume"
+###     nodeInstallRoot: Target install folder (for example "C:\Hadoop")
+###     serviceCredential: Credential object used for service creation
+###     configs:
+###
+###############################################################################
+function Configure(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $component,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $nodeInstallRoot,
+    [System.Management.Automation.PSCredential]
+    [Parameter( Position=2, Mandatory=$false )]
+    $serviceCredential,
+    [hashtable]
+    [parameter( Position=3 )]
+    $configs = @{},
+    [bool]
+    [parameter( Position=4 )]
+    $aclAllFolders = $True
+    )
+{
+
+    if ( $component -eq "flume" )
+    {
+        Write-Log "Configure: flume does not have any configurations"
+    }
+    else
+    {
+        throw "Configure: Unsupported compoment argument."
+    }
+}
+
+
+### Helper routing that converts a $null object to nothing. Otherwise, iterating over
+### a $null object with foreach results in a loop with one $null element.
+function empty-null($obj)
+{
+   if ($obj -ne $null) { $obj }
+}
+
+### Gives full permissions on the folder to the given user
+function GiveFullPermissions(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $folder,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $username,
+    [bool]
+    [Parameter( Position=2, Mandatory=$false )]
+    $recursive = $false)
+{
+    Write-Log "Giving user/group `"$username`" full permissions to `"$folder`""
+    $cmd = "icacls `"$folder`" /grant ${username}:(OI)(CI)F"
+    if ($recursive) {
+        $cmd += " /T"
+    }
+    Invoke-CmdChk $cmd
+}
+
+### Checks if the given space separated roles are in the given array of
+### supported roles.
+function CheckRole(
+    [string]
+    [parameter( Position=0, Mandatory=$true )]
+    $roles,
+    [array]
+    [parameter( Position=1, Mandatory=$true )]
+    $supportedRoles
+    )
+{
+    foreach ( $role in $roles.Split(" ") )
+    {
+        if ( -not ( $supportedRoles -contains $role ) )
+        {
+            throw "CheckRole: Passed in role `"$role`" is outside of the supported set `"$supportedRoles`""
+        }
+    }
+}
+
+### Creates and configures the service.
+function CreateAndConfigureHadoopService(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $service,
+    [String]
+    [Parameter( Position=1, Mandatory=$true )]
+    $hdpResourcesDir,
+    [String]
+    [Parameter( Position=2, Mandatory=$true )]
+    $serviceBinDir,
+    [System.Management.Automation.PSCredential]
+    [Parameter( Position=3, Mandatory=$true )]
+    $serviceCredential
+)
+{
+    if ( -not ( Get-Service "$service" -ErrorAction SilentlyContinue ) )
+    {
+		 Write-Log "Creating service `"$service`" as $serviceBinDir\$service.exe"
+        $xcopyServiceHost_cmd = "copy /Y `"$HDP_RESOURCES_DIR\serviceHost.exe`" `"$serviceBinDir\$service.exe`""
+        Invoke-CmdChk $xcopyServiceHost_cmd
+
+        #Creating the event log needs to be done from an elevated process, so we do it here
+        if( -not ([Diagnostics.EventLog]::SourceExists( "$service" )))
+        {
+            [Diagnostics.EventLog]::CreateEventSource( "$service", "" )
+        }
+
+        Write-Log "Adding service $service"
+        $s = New-Service -Name "$service" -BinaryPathName "$serviceBinDir\$service.exe" -Credential $serviceCredential -DisplayName "Apache Hadoop $service"
+        if ( $s -eq $null )
+        {
+            throw "CreateAndConfigureHadoopService: Service `"$service`" creation failed"
+        }
+
+        $cmd="$ENV:WINDIR\system32\sc.exe failure $service reset= 30 actions= restart/5000"
+        Invoke-CmdChk $cmd
+
+        $cmd="$ENV:WINDIR\system32\sc.exe config $service start= disabled"
+        Invoke-CmdChk $cmd
+
+        Set-ServiceAcl $service
+    }
+    else
+    {
+        Write-Log "Service `"$service`" already exists, Removing `"$service`""
+        StopAndDeleteHadoopService $service
+        CreateAndConfigureHadoopService $service $hdpResourcesDir $serviceBinDir $serviceCredential
+    }
+}
+
+### Stops and deletes the Hadoop service.
+function StopAndDeleteHadoopService(
+    [String]
+    [Parameter( Position=0, Mandatory=$true )]
+    $service
+)
+{
+    Write-Log "Stopping $service"
+    $s = Get-Service $service -ErrorAction SilentlyContinue
+
+    if( $s -ne $null )
+    {
+        Stop-Service $service
+        $cmd = "sc.exe delete $service"
+        Invoke-Cmd $cmd
+    }
+}
+
+###
+### Public API
+###
+Export-ModuleMember -Function Install
+Export-ModuleMember -Function Uninstall
+Export-ModuleMember -Function Configure
+Export-ModuleMember -Function StartService
+Export-ModuleMember -Function StopService
